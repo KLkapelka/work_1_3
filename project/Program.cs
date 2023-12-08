039b938ecdbe15c302f51eac0191cb4f91f2796b
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 05:25:13 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,42 @@
+﻿// Добавляйте новые элементы в список до тех пор, пока пользователь не отправит пустую строку.
+// Выведите в консоль самый короткий и самый длинный элементы списка.
+
+using System;
+using System.Collections.Generic; 
+class work_1_3
+{
+    static void Main()
+    {
+        List<string> array = new List<string>(); 
+        
+        while (true) 
+        {
+            Console.Write("Введите число: "); 
+            string data = Console.ReadLine(); 
+            if (string.IsNullOrEmpty(data)) 
+            {
+                break; 
+            }
+            array.Add(data); 
+        }
+        
+        string shortest = array[0]; 
+        string longest = array[0]; 
+        for (int i = 0; i < array.Count; i++)
+        {
+            if (array[i].Length < shortest.Length) 
+            {
+                shortest = array[i]; 
+            }
+
+            if (array[i].Length > longest.Length) 
+            {
+                longest = array[i]; 
+            }
+        }
+        
+        var str = string.Join(" ", array); 
+        Console.WriteLine(str); 
+        Console.WriteLine($"Самый короткий элемент: {shortest} \t Самый длинный элемент: {longest}"); s
+    }
+}
